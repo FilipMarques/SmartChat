19c8690dcd365b190a36163a5f95b2a5205b1b0a
Filipe Camargo Marques <filipe.marques@compasso.com.br>
2024-12-16 19:00:33 -0300

    First commit

---
+++ SmartChat/SmartChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartChatApp.swift
+//  SmartChat
+//
+//  Created by Filipe Camargo Marques on 16/12/24.
+//
+
+import SwiftUI
+
+@main
+struct SmartChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SmartChatTests/SmartChatTests.swift
@@ -0,0 +1,17 @@
+//
+//  SmartChatTests.swift
+//  SmartChatTests
+//
+//  Created by Filipe Camargo Marques on 16/12/24.
+//
+
+import Testing
+@testable import SmartChat
+
+struct SmartChatTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
