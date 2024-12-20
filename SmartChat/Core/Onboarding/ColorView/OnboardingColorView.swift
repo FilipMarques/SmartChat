5ea52d2548233e83efe4e782e7a5b928fe908bd8
Filipe Camargo Marques <filipe.marques@compasso.com.br>
2024-12-20 17:02:30 -0300

    [Feature] - OnboardingIntroView and OnboardingColorView

---
+++ SmartChat/Core/Onboarding/ColorView/OnboardingColorView.swift
@@ -0,0 +1,18 @@
+//
+//  OnboardingColorView.swift
+//  SmartChat
+//
+//  Created by Filipe Camargo Marques on 20/12/24.
+//
+
+import SwiftUI
+
+struct OnboardingColorView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OnboardingColorView()
+}
---
+++ SmartChat/Core/Onboarding/IntroView/OnboardingIntroView.swift
@@ -0,0 +1,18 @@
+//
+//  OnboardingIntroView.swift
+//  SmartChat
+//
+//  Created by Filipe Camargo Marques on 20/12/24.
+//
+
+import SwiftUI
+
+struct OnboardingIntroView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OnboardingIntroView()
+}
