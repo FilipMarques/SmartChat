781f93600b9fabfb9503c151eb6c5a3fde419c9b
Filipe Camargo Marques <filipe.marques@compasso.com.br>
2024-12-22 13:54:17 -0300

    [Feature] - HeroCellView

---
+++ SmartChat/Components/Views/HeroCellView.swift
@@ -0,0 +1,18 @@
+//
+//  HeroCellView.swift
+//  SmartChat
+//
+//  Created by Filipe Camargo Marques on 22/12/24.
+//
+
+import SwiftUI
+
+struct HeroCellView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HeroCellView()
+}
