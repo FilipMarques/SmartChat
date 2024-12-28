f29acd31c1ee51f16490f4a53e8652ffe9685152
Filipe Camargo Marques <filipe.marques@compasso.com.br>
2024-12-28 17:36:50 -0300

    Feature - Add category Cell

---
+++ SmartChat/Components/ViewModifiers/ButtonViewModifiers.swift
@@ -0,0 +1,18 @@
+//
+//  ButtonViewModifiers.swift
+//  SmartChat
+//
+//  Created by Filipe Camargo Marques on 28/12/24.
+//
+
+import SwiftUI
+
+struct ButtonViewModifiers: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ButtonViewModifiers()
+}
