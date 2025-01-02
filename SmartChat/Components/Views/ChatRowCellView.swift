23874052290f750206801823567afca446c12358
Filipe Camargo Marques <filipe.marques@compasso.com.br>
2025-01-02 20:16:53 -0300

    [Feature] ChatRowCellView

---
+++ SmartChat/Components/Views/ChatRowCellView.swift
@@ -0,0 +1,18 @@
+//
+//  ChatRowCellView.swift
+//  SmartChat
+//
+//  Created by Filipe Camargo Marques on 02/01/25.
+//
+
+import SwiftUI
+
+struct ChatRowCellView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChatRowCellView()
+}
---
+++ SmartChat/Components/Views/ChatRowCellViewBuilder.swift
@@ -0,0 +1,18 @@
+//
+//  ChatRowCellViewBuilder.swift
+//  SmartChat
+//
+//  Created by Filipe Camargo Marques on 02/01/25.
+//
+
+import SwiftUI
+
+struct ChatRowCellViewBuilder: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChatRowCellViewBuilder()
+}
